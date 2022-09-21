a516dc0f600c6ee66c31cd13476ee8c384b029e3
nikob767 <niko.nb61@gmail.com>
2022-09-20 21:17:23 -0300

    creada funcion desencriptar

--- encriptador/homepage_script.js
+++ encriptador/homepage_script.js
@@ -6,9 +6,17 @@ var parrafo = document.querySelector(".parrafo")
 var resultado = document.querySelector(".resultadoDesencriptado")
 
 //imprime el resultado ya encriptado//
-function imprimirResultado() {
+function encriptar() {
     ocultarElementos();
+    
     resultado.textContent = encriptarTexto(textoingresado());
+       
+}
+
+function desencriptar() {
+    ocultarElementos();
+   
+    resultado.textContent = desencriptarTexto(textoingresado());
        
 }
 
@@ -46,15 +54,52 @@ function encriptarTexto(mensaje){
         }
         else {
             final = final + texto[i];
-        } 
-        
+        }                 
+    
+    }   
+    
+    return final;
+
+} 
+
+function desencriptarTexto(mensaje){
+    var texto = mensaje;
+    var final = ""; //mostrara el resultado final//
+    
+    for (var i = 0 ; i < texto.length; i++){
+        if (texto[i] == "a"){
+            final = final + "a"
+            i = i + 1;
+        }
+        else if (texto[i] == "e"){
+            final = final + "e"
+            i = i + 4;
+        }  
+        else if (texto[i] == "i"){
+            final = final + "i"
+            i = i + 3;
+        }  
+        else if (texto[i] == "o"){ 
+            final = final + "o"
+            i = i + 3;
+        }  
+        else if (texto[i] == "u"){
+            final = final + "u"
+            i = i + 3;
+        }
+        else {
+            final = final + texto[i];            
+        }     
                 
     
     }   
     
     return final;
 
- } 
+} 
 
 
-botonEncriptar.onclick = imprimirResultado;
+
+
+botonEncriptar.onclick = encriptar;
+botonDesencriptar.onclick = desencriptar;
